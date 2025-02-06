0976eb5f845075e9e835f9a79e3234cf97bc9cc5
Medaminesagh1 <147838684+Medaminesagh1@users.noreply.github.com>
2025-02-06 16:27:50 +0100

    ajout mutation acheterQtProduit

--- isiscapitalist_backend/src/resolver.ts
+++ isiscapitalist_backend/src/resolver.ts
@@ -8,6 +8,51 @@ constructor(private service: AppService) {}
 @Query()
 async getWorld(@Args('user') user: string) {
 const world = this.service.readUserWorld(user);
-return world;
+this.service.saveWorld(user, world);
+return world
 }
+@Mutation()
+async acheterQtProduit(
+@Args('user') user: string,
+@Args('id') id: number,
+@Args('quantite') quantite: number,
+) {
+  const world = this.service.readUserWorld(user);
+  // Trouver le produit
+  const product = world.products.find((p) => p.id === id);
+  if(!product){
+      throw new Error(`Le produit avec l'id ${id} n'existe pas`)
+  }
+
+   // Calculer le coût total de l'achat
+     let totalCost = 0;
+     let currentCost = product.cout;
+
+     for (let i = 0; i < quantite; i++) {
+       totalCost += currentCost;
+       currentCost *= product.croissance; // Appliquer l'augmentation du coût pour chaque unité
+       currentCost=Math.round(currentCost*100)/100;
+     }
+
+     // Vérifier si l'utilisateur a assez d'argent
+     if (world.money < totalCost) {
+       throw new Error("Pas assez d'argent pour cet achat.");
+     }
+
+     // Augmenter la quantité du produit
+     product.quantite += quantite;
+
+     // Déduire l'argent du monde
+     world.money -= totalCost;
+
+     // Mettre à jour le coût d'achat du produit pour la prochaine unité
+     product.cout = currentCost;
+
+     // Sauvegarder le monde
+     this.service.saveWorld(user, world);
+
+     // Retourner le produit mis à jour
+     return product;
+   }
+
 }
